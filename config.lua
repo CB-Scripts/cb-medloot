7c61f866e0b250eb604fe6481abd5a966b82676c
Joel <34402846+Qwerty1Verified@users.noreply.github.com>
2022-09-22 18:30:45 +0100

    Feat: Config table models

--- config.lua
+++ config.lua
@@ -2,6 +2,13 @@ Config = {}
 
 Config.RandomItemAmount = 3
 
+Config.Models = {
+    "v_med_trolley",
+    "v_med_trolley2",
+    "prop_medstation_01",
+    "v_med_cor_cemtrolly"
+}
+
 Config.RewardItems = {
     'painkillers',
     'ifaks',
